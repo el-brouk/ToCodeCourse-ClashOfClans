e466969b4091d3deb44ac3fb00f001e5d3571450
Liza Brouk <lizavetka.b@gmail.com>
2024-10-06 13:42:19 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper">
+    <HeaderBase :title="title" />
+    <div class="wrapper-content">
+      <div class="container">
+        <!-- <h1>Hello</h1> -->
+        <router-view />
+      </div>
+    </div>
+    <FooterBase />
+  </div>
+</template>
+
+<script>
+import HeaderBase from "@/components/HeaderLayout.vue"
+import FooterBase from "@/components/FooterLayout.vue"
+
+export default {
+  components: { HeaderBase, FooterBase },
+
+  data() {
+    return {
+      title: "MyTitle",
+    }
+  },
+}
+</script>
+
+<style lang="scss"></style>
